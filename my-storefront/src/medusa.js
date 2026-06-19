e5dd3bb3bf4db1fe76818dc72ca060a567b535b0
jude <96506765+jude5678@users.noreply.github.com>
2026-06-18 22:42:09 -0400

    update medusa.js

--- my-storefront/src/medusa.js
+++ my-storefront/src/medusa.js
@@ -3,7 +3,7 @@ import Medusa from "@medusajs/js-sdk";
 const medusa = new Medusa({ 
   baseUrl: "http://localhost:9000", 
   maxRetries: 3,
-  publishableKey: 'pk_0b45406e09276b3cdb1c28630d0054f8ae129687e21c9be359c76adf65ef67f4'
+  publishableKey: import.meta.env.VITE_MEDUSA_PUBLISHABLE_KEY
 });
 
 export default medusa;
