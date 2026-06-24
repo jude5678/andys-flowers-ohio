c58f0905836dc392b521cdebfeaeed429e7b993d
jude <96506765+jude5678@users.noreply.github.com>
2026-06-24 03:33:42 -0400

    update sdk.js

--- my-storefront/src/lib/sdk.js
+++ my-storefront/src/lib/sdk.js
@@ -5,6 +5,6 @@ const MEDUSA_BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL || "https://m
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   debug: import.meta.env.DEV,
-  publishableKey: import.meta.env.VITE_MEDUSA_PUBLISHABLE_KEY || "pk_0b45406e09276b3cdb1c28630d0054f8ae129687e21c9be359c76adf65ef67f4"
+  publishableKey: import.meta.env.VITE_MEDUSA_PUBLISHABLE_KEY 
 })
 
