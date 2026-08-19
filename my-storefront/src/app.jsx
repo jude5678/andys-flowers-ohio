fba0da5674a80c1508118681d4b146f055357b4f
jude <96506765+jude5678@users.noreply.github.com>
2026-08-19 00:10:31 -0400

    update main.css

--- my-storefront/src/app.jsx
+++ my-storefront/src/app.jsx
@@ -338,8 +338,24 @@ export default function App() {
         {/* Global Application Footer Markup */}
         <footer id="footer">
           <ul className="icons">
-            <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+            <li>
+              <a href="https://facebook.com" className="icon" aria-label="Facebook" target="_blank" rel="noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style={{ fill: 'currentColor', width: '20px', height: '20px' }}>
+                  <path d="M80 299.3V256H12v-54.7h68v-39.7c0-67.4 41.1-104 101.2-104 28.8 0 53.5 2.1 60.7 3v70.5h-41.8c-32.7 0-39 15.5-39 38.3v50.2h78l-10.2 54.7h-67.8V448H164V299.3H80z"/>
+                </svg>
+                <span className="label">Facebook</span>
+              </a>
+            </li> 
+
+            <li>
+              <a href="https://instagram.com" className="icon" aria-label="Instagram" target="_blank" rel="noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{ fill: 'currentColor', width: '20px', height: '20px' }}>
+                  <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                </svg>
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+
             <li>
               <a href="#" className="icon brands fa-envelope">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -100 512 512" style={{ fill: 'currentColor' }}>
